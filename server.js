e1677d7bb2b5823e39b6b1245819eefbafd3d622
KuronaiZuko1 <lebogangmosako167@gmail.com>
2025-05-24 07:34:29 +0200

    Auto-update

--- server.js
+++ server.js
@@ -13,3 +13,6 @@ app.use('/api/license', require('./routes/license'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
